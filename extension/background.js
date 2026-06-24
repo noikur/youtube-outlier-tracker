58b3b5b5bb0e87ca7be4595fff4301532815667d
nelson idahosa <145884395+noikur@users.noreply.github.com>
2026-06-24 16:36:19 +0100

    Clean up comments in background.js
    Removed comments about the service worker and future extensions.

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,5 @@
 /**
  * background.js -- Manifest V3 service worker.
- *
- * Minimal for now. In MV3, extensions require a service worker even if
- * it does very little. All the real work happens in content.js (which
- * runs on YouTube pages) and the local Python API server.
- *
- * This is where you'd add things like scheduled badge refresh, cross-tab
- * state syncing, or notifications when outliers are detected -- all future
- * extensions of the current architecture.
  */
 
 chrome.runtime.onInstalled.addListener(() => {
